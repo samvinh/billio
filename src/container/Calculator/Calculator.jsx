193543f0cc0fdcd7b02b80ef7b6f2092173090e0
Sam Vinh <samvinh0x@gmail.com>
2023-02-24 00:26:21 -0800

    Implement bill splitting and option toggling

--- src/container/Calculator/Calculator.jsx
+++ src/container/Calculator/Calculator.jsx
@@ -5,6 +5,8 @@ import Button from '@mui/material/Button';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
 
 import 'ag-grid-community/styles/ag-grid.css' // Core grid CSS, always needed
 import 'ag-grid-community/styles/ag-theme-alpine.css' // Optional theme CSS
@@ -47,6 +49,11 @@ const printResult = (res) => {
 
 const Calculator = () => {
   const [billName, setBillName] = useState('')
+
+  const [options, setOptions] = useState(false)
+  const [splitDivisor, setSplitDivisor] = useState(1)
+  const [splitAmount, setSplitAmount] = useState(null)
+
   const [discountPercentage, setDiscountPercentage] = useState(0)
   const [discount, setDiscount] = useState(null)
 
@@ -57,6 +64,7 @@ const Calculator = () => {
 
   const [subtotal, setSubtotal] = useState(0)
   const [total, setTotal] = useState(0)
+
   const [rowData, setRowData] = useState([])
   const [columnDefs] = useState([
     {field: 'quantity', flex: 1, minWidth: 100, editable: true, valueParser: params => Number(params.newValue)},
@@ -94,20 +102,30 @@ const Calculator = () => {
       let rawTax = (rawSubtotal - rawDiscount) * (taxPercentage / 100)
       let roundedTax = Math.round(rawTax * 100) / 100
       setTax(roundedTax);
-      let rawTotal = (rawSubtotal - rawDiscount) +  rawTax  + Number(tip);
+      let rawTotal = (rawSubtotal - rawDiscount) +  rawTax  + Number(tip)
       let roundedTotal = Math.round(rawTotal * 100) / 100
       setTotal(roundedTotal)
+      if(splitDivisor > 0){
+        let rawSplitAmount = rawTotal / splitDivisor
+        let roundedSplitAmount = Math.round(rawSplitAmount * 100) / 100
+        setSplitAmount(roundedSplitAmount)
+      }
     } else {
       setDiscount(null)
       let rawTax = rawSubtotal * (taxPercentage / 100)
       let roundedTax = Math.round(rawTax * 100) / 100
       setTax(roundedTax);
-      let rawTotal = rawSubtotal +  rawTax  + Number(tip);
+      let rawTotal = rawSubtotal +  rawTax  + Number(tip)
       let roundedTotal = Math.round(rawTotal * 100) / 100
       setTotal(roundedTotal)
+      if(splitDivisor > 0){
+        let rawSplitAmount = rawTotal / splitDivisor
+        let roundedSplitAmount = Math.round(rawSplitAmount * 100) / 100
+        setSplitAmount(roundedSplitAmount)
+      }
     }
   }
-  }, [taxPercentage, discountPercentage, tip])
+  }, [taxPercentage, discountPercentage, tip, splitDivisor])
 
 
   //Persisting data between refresh
@@ -117,12 +135,14 @@ const Calculator = () => {
     const discountPercentage = window.localStorage.getItem('discountPercentage') != null ? window.localStorage.getItem('discountPercentage') : 0
     const taxPercentage = window.localStorage.getItem('taxPercentage') != null ? window.localStorage.getItem('taxPercentage') : 0
     const tip = window.localStorage.getItem('tip') != null ? window.localStorage.getItem('tip') : 0
-    
+    const splitDivisor = window.localStorage.getItem('splitDivisor') != null ? window.localStorage.getItem('splitDivisor') : 1
+
     setBillName(billName)
     setRowData(rowData)
     setDiscountPercentage(discountPercentage)
     setTaxPercentage(taxPercentage)
     setTip(tip)
+    setSplitDivisor(splitDivisor)
   }, []);
 
   useEffect(() => {
@@ -134,8 +154,10 @@ const Calculator = () => {
     window.localStorage.setItem('discountPercentage', discountPercentage);
     window.localStorage.setItem('taxPercentage', taxPercentage);
     window.localStorage.setItem('tip', tip);
+    window.localStorage.setItem('splitDivisor', splitDivisor);
+
     calculateAll()
-  }, [taxPercentage, discountPercentage, tip, calculateAll])
+  }, [taxPercentage, discountPercentage, tip, splitDivisor, calculateAll])
 
   const gridRef = useRef()
   const gridStyle = useMemo(() => ({ height: '100%', width: '100%', }), [])
@@ -161,11 +183,13 @@ const Calculator = () => {
     calculateAll()
     localStorage.clear();
     setBillName('')
-    setDiscount(0)
+    setDiscount(null)
     setDiscountPercentage(0)
     setTax(0)
     setTaxPercentage(0)
     setTip(0)
+    setSplitDivisor(1)
+    setSplitAmount(null)
   }, [calculateAll])
 
   const addItem = useCallback((addIndex) => {
@@ -183,26 +207,38 @@ const Calculator = () => {
     const selectedData = gridRef.current.api.getSelectedRows()
     gridRef.current.api.applyTransaction({ remove: selectedData })
     calculateAll()
-  }, [])
+  }, [calculateAll])
+
+  const handleBillNameChange = (event) => {
+    setBillName(event.target.value)
+  }
+
+  const handleOptionsChange = (event) => {
+    setOptions(event.target.checked)
+  }
 
   const handleTaxPercentageChange = (event) => {
-    setTaxPercentage(event.target.value);
+    setTaxPercentage(event.target.value)
   }
 
   const handleDiscountPercentageChange = (event) => {
-    setDiscountPercentage(event.target.value);
+    setDiscountPercentage(event.target.value)
   }
 
   const handleTipChange = (event) => {
-    setTip(event.target.value);
+    if(event.target.value < 0) setTip(0)
+    else setTip(event.target.value)
   }
 
-  const handleBillNameChange = (event) => {
-    setBillName(event.target.value);
+  const handleSplitDivisorChange = (event) => {
+    if(event.target.value < 1 && event.target.value.length >= 1) setSplitDivisor(1)
+    else setSplitDivisor(event.target.value)
   }
 
+  let splitDivisorText = splitDivisor > 1 ? `Split ${splitDivisor}-ways` : `Split ${splitDivisor}-way`
 
   return (
+    <ThemeProvider theme={theme}>
     <div className='calculator-container'>
       <div style={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
           <TextField
@@ -213,10 +249,8 @@ const Calculator = () => {
             onChange={handleBillNameChange}
           />
         <div style={{ display: 'flex', marginBottom: '20px', marginTop: '20px', justifyContent: 'space-between' }}>
-        <ThemeProvider theme={theme}>
           <Button variant='outlined' color='primary' onClick={clearData}>Clear Data</Button>
           <Button variant='outlined' color='danger' onClick={onRemoveSelected}>Remove Selected</Button>
-        </ThemeProvider>
         </div>
         <div style={{ flexGrow: '1' }}>
           <div style={gridStyle} className="ag-theme-alpine">
@@ -236,66 +270,80 @@ const Calculator = () => {
         </div>
       </div>
       <div style={{ display: 'flex', justifyContent: 'center', margin: '10px'}}>
-        <ThemeProvider theme={theme}>
-          <Button sx={{ ml: '2em', mr:'2em', mt: '1em'}} variant='standard' color='neutral' onClick={() => deselect()}>Unselect All</Button>
-          <Button sx={{ ml: '2em', mr:'2em', mt: '1em'}} variant='contained' color='primary' startIcon={<AddCircleIcon />} onClick={() => addItem(undefined)}>Add Item</Button>
-          {/* <Button variant='contained' color='neutral' onClick={() => getRowData()}>Data</Button> */}
-        </ThemeProvider>
+        <Button sx={{ ml: '2em', mr:'2em', mt: '1em'}} variant='standard' color='neutral' onClick={() => deselect()}>Unselect All</Button>
+        <Button sx={{ ml: '2em', mr:'2em', mt: '1em'}} variant='contained' color='primary' startIcon={<AddCircleIcon />} onClick={() => addItem(undefined)}>Add Item</Button>
+        {/* <Button variant='contained' color='neutral' onClick={() => getRowData()}>Data</Button> */}
       </div>
       <div style={{display: 'flex', flexDirection:'column', marginLeft: '20px', marginRight: '20px', minWidth: '40px', maxWidth: '140px'}}>
-        <TextField
-          id="discount-percentage"
-          value={discountPercentage}
-          type="number"
-          variant="standard"
-          label="Discount percent"
-          InputProps={{
-            endAdornment: <InputAdornment position="end">%</InputAdornment>,
-          }}
-          inputProps={{
-            min: 0, 
-            step: '.1'
-          }}
-          onChange={handleDiscountPercentageChange}
-        />
-        <TextField
-          id="tax-percentage"
-          value={taxPercentage}
-          type="number"
-          variant="standard"
-          label="Tax percent"
-          InputProps={{
-            endAdornment: <InputAdornment position="end">%</InputAdornment>,
-          }}
-          inputProps={{
-            min: 0, 
-            step: '.1'
-          }}
-          onChange={handleTaxPercentageChange}
-        />
-        <TextField
-          id="tip"
-          value={tip}
-          type="number"
-          variant="standard"
-          label="Tip"
-          InputProps={{
-            startAdornment: <InputAdornment position="start">$</InputAdornment>,
-          }}
-          inputProps={{
-            min: 0, 
-            step: '.01'
-          }}
-          onChange={handleTipChange}
-        />
+        <FormControlLabel color='primary' control={<Switch value={options} onChange={handleOptionsChange}/>} label="Options" />
+        {options && <div>
+          <TextField
+            id="discount-percentage"
+            value={discountPercentage}
+            type="number"
+            variant="standard"
+            label="Discount percent"
+            InputProps={{
+              endAdornment: <InputAdornment position="end">%</InputAdornment>,
+            }}
+            inputProps={{
+              min: 0, 
+              step: '.1'
+            }}
+            onChange={handleDiscountPercentageChange}
+          />
+          <TextField
+            id="tax-percentage"
+            value={taxPercentage}
+            type="number"
+            variant="standard"
+            label="Tax percent"
+            InputProps={{
+              endAdornment: <InputAdornment position="end">%</InputAdornment>,
+            }}
+            inputProps={{
+              min: 0, 
+              step: '.1'
+            }}
+            onChange={handleTaxPercentageChange}
+          />
+          <TextField
+            id="tip"
+            value={tip}
+            type="number"
+            variant="standard"
+            label="Tip"
+            InputProps={{
+              startAdornment: <InputAdornment position="start">$</InputAdornment>,
+            }}
+            inputProps={{
+              min: 0, 
+              step: '.01'
+            }}
+            onChange={handleTipChange}
+          />
+          <TextField
+            id="splitDivisor"
+            value={splitDivisor}
+            type="number"
+            variant="standard"
+            label={splitDivisorText}
+            inputProps={{
+              min: 1, 
+            }}
+            onChange={handleSplitDivisorChange}
+          />
+        </div>}
       </div>
       <div className='calculator-cost-breakdown'>
         <Typography variant='body1'>Subtotal: ${subtotal.toFixed(2)}</Typography>
         {discount && <Typography variant='body1'>Discount: -${discount.toFixed(2)}</Typography>}
         <Typography variant='body1'>Tax: ${tax.toFixed(2)}</Typography>
         <Typography variant='body1'><strong>Total: ${total.toFixed(2)}</strong></Typography>
+        {splitAmount > 0 && splitDivisor.length > 0 && <Typography variant='body1'>Split: ${splitAmount.toFixed(2)}</Typography>}
       </div>
     </div>
+    </ThemeProvider>
   )
 }
 
