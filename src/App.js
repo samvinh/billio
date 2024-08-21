a3d049b3b8503725989da418f202579a70ab7449
Sam Vinh <samvinh0x@gmail.com>
2024-08-20 23:18:33 -0700

    Remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import "./App.css";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
 import { Calculator } from "./container";
 import { Header } from "./container";
 
